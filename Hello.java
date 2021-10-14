bf8338f02ab06258111dba39a5226cd86c9d92ca
unknown <gayoyawalkar@gmail.com>
2021-10-14 07:17:26 +0530

    Welcome to BST program

---
+++ Hello.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.practice;
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Binary Search Tree Data Structure Program");
+	}
+}
